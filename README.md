a46f4a8498d221c279922ed6c2f7cc59caf52270
Mike Dowd <mikedowd1972@gmail.com>
2017-12-16 00:01:17 -0700

    changed the readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ambra-bonus-points
-For your eyes only Ambra, I hope you like it
+
+Hi, Ambra people, this is my node tree that you suggested I create for some bonus points. I hope you like it.
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Implement a 3-level node structure of parents and children using JavaScript. Each node should store the parent name and a list of children. Create some node data, then using this, represent the data as a tree structure on a web page. You should be able to collapse and expand the tree structure by clicking node names on the web page. You are allowed to use plain JavaScript or your favorite library.
+
 let node = (name, parent) => {
   return {
     name,
